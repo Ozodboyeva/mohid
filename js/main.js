c53825a93ab53418964f6c1698da23a85678f150
MuharramOzodboyeva <muharramozodboyeva598@gmail.com>
2023-07-05 13:28:07 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+let modeBtn = document.getElementById("dark-light");
+
+modeBtn.addEventListener("click", function () {
+  if (document.body.className != "dark") {
+    this.firstElementChild.src = "images/light.svg";
+  } else {
+    this.firstElementChild.src = "images/dark.svg";
+  }
+  document.body.classList.toggle("dark");
+});
+
+function openNavbar() {
+  document.getElementById("navbar-responsive").style.left = "0";
+  // document.getElementById("navbar-responsive").style.top = "0";
+}
+function closeNavbar() {
+  document.getElementById("navbar-responsive").style.left = "-100%";
+  // document.getElementById("navbar-responsive").style.top = "-100%";
+}
+
+document.getElementById("navbar-open").addEventListener("click", openNavbar);
+document.getElementById("navbar-close").addEventListener("click", closeNavbar);
